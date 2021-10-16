6ba6c0e8c73eb9b0f4988c4b8f0451372896d093
JacobTTorres <jacobttorres@hotmail.com>
2021-10-16 11:43:19 -0500

    rendering and changing title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import FreeSubscription from "./components/freeSubscription/FreeSubscription";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <FreeSubscription />
+    </div>
+  );
 }
 
 export default App;
