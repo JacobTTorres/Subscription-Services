8f9c69f4f76ace0a5a9dbdeca4d3a60b0b431e25
JacobTTorres <jacobttorres@hotmail.com>
2021-10-16 11:33:37 -0500

    Getting started

---
+++ src/components/freeSubscription/FreeSubscription.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./freeSubscription.css";
+
+export default function FreeSubscription() {
+  return (
+    <div>
+      <h1>Free Subscription Component</h1>
+    </div>
+  );
+}
---
+++ src/components/premiumSubscription/PremiumSubscription.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./premiumSubscription.css";
+
+export default function PremiumSubscription() {
+  return (
+    <div>
+      <h1>Premium Subscription Component</h1>
+    </div>
+  );
+}
