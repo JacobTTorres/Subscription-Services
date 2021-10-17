80a9826b4ecfbc8da63b2a805db423e921970a6e
JacobTTorres <jacobttorres@hotmail.com>
2021-10-17 16:31:46 -0500

    still needs work

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 import "./footer.css";
 
 export default function Footer() {
   return (
     <section className="footerContainer">
-      <div>
-        <h1>Footer Component</h1>
+      <div className="contactContainer">
+        <div className="gitHub">
+          <FontAwesomeIcon icon={faCoffee} />
+        </div>
+        <div className="email">
+          <FontAwesomeIcon icon={faCoffee} />
+        </div>
       </div>
     </section>
   );
