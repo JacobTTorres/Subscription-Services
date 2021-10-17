ca6155a5ff0a6d9fdc592a187ab44cd31a869408
JacobTTorres <jacobttorres@hotmail.com>
2021-10-17 17:12:00 -0500

    needs some touches

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faCode } from "@fortawesome/free-solid-svg-icons";
 
 import "./footer.css";
 
@@ -9,10 +9,12 @@ export default function Footer() {
     <section className="footerContainer">
       <div className="contactContainer">
         <div className="gitHub">
-          <FontAwesomeIcon icon={faCoffee} />
+          <FontAwesomeIcon icon={faCode} />
+          https://github.com/JacobTTorres/Subscription-Services
         </div>
         <div className="email">
-          <FontAwesomeIcon icon={faCoffee} />
+          <FontAwesomeIcon icon={faEnvelope} />
+          jacobttorres@hotmail.com
         </div>
       </div>
     </section>
