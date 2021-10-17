8603aeee7e8429fef13f4b6d20ef503c141ec968
JacobTTorres <jacobttorres@hotmail.com>
2021-10-17 13:57:27 -0500

    Premium Subscription done?

---
+++ src/components/premiumPic/PremiumPic.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./premiumPic.css";
+
+export default function PremiumPic() {
+  return (
+    <div className="image">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnGb_pLbP9s0ieI_cb03L4OyYi03MdJiscxQ&usqp=CAU"
+        alt="Premium Subscription"
+        className="pic"
+      />
+    </div>
+  );
+}
--- src/components/premiumSubscription/PremiumSubscription.js
+++ src/components/premiumSubscription/PremiumSubscription.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PremiumPic from "../premiumPic/PremiumPic";
 
 import "./premiumSubscription.css";
 
@@ -6,7 +7,23 @@ export default function PremiumSubscription() {
   return (
     <section className="premiumSubContainer">
       <div>
-        <h1>Premium Subscription Component</h1>
+        <h1 className="title">Premium Subscription</h1>
+        <PremiumPic className="picture" />
+        <ul
+          className="listItem"
+          style={{ flex: 1, flexWrap: "wrap", inlineSize: "30vw" }}
+        >
+          <li className="item">Online Chord Book & Scale Chart</li>
+          <li className="item">Online tabs</li>
+          <li className="item">Play along tracks </li>
+          <li className="item">
+            Online lessons with any of our Professionals (unlimited)
+          </li>
+        </ul>
+        <h3 className="price">$15.99 per month</h3>
+        <div className="buttonContainer">
+          <button className="button">Subscribe</button>
+        </div>
       </div>
     </section>
   );
